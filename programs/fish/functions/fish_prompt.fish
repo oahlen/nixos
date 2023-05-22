18022bddb427b007b238499e68b0ea6c9ddfd2e1
Oscar Ahlén <github@ahlen.dev>
2023-05-22 19:24:29 +0200

    Update fish prompt

--- programs/fish/functions/fish_prompt.fish
+++ programs/fish/functions/fish_prompt.fish
@@ -1,4 +1,7 @@
 function fish_prompt --description "Write out the prompt"
+    set -lx fish_prompt_pwd_dir_length 1
+    set -lx fish_prompt_pwd_full_dirs 3
+
     echo -s (set_color $fish_color_cwd)(prompt_pwd) (set_color normal)" on"(fish_vcs_prompt)
     echo -n -s (set_color $fish_color_prefix)"> "
 end
