9a2a0f3de37dd0f0c48700fedc3552172ba9aba5
Oscar Ahlén <github@ahlen.dev>
2023-06-07 20:00:08 +0200

    Lock to 23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     home-manager = {
       url = "github:nix-community/home-manager";
