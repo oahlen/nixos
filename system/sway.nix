7819920c62e458b9856a4473252fcb267964b0f7
Oscar Ahlén <github@ahlen.dev>
2023-07-04 16:48:33 +0200

    Add polkit to sway module

--- system/sway.nix
+++ system/sway.nix
@@ -17,6 +17,7 @@
       grim
       pavucontrol
       playerctl
+      polkit_gnome
       slurp
       swayimg
       swaylock
@@ -36,8 +37,27 @@
 
   services.dbus.enable = true;
 
-  services.gnome.gnome-keyring.enable = true;
-  programs.ssh.startAgent = true;
-
   sound.mediaKeys.enable = true; # Should not be enabled in Gnome, KDE, etc
+
+  security.polkit.enable = true;
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+
+  programs.ssh.startAgent = true;
 }
