355ecdae138f6c11fdfed0fe2e98ba936679a3ab
Oscar Ahlén <github@ahlen.dev>
2023-07-05 17:07:57 +0200

    Add cursor config

--- hosts/xps15/home.nix
+++ hosts/xps15/home.nix
@@ -11,6 +11,16 @@
     };
   };
 
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.gnome.adwaita-icon-theme;
+    size = 24;
+    x11 = {
+      enable = true;
+      defaultCursor = "Adwaita";
+    };
+  };
+
   home.packages = with pkgs; [
     dbeaver
     firefox
