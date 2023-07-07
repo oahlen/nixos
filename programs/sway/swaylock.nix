11cc558278badc1de1549d1e920ff0564d6be225
Oscar Ahlén <github@ahlen.dev>
2023-07-07 10:40:50 +0200

    Update swaylock settings

--- programs/sway/swaylock.nix
+++ programs/sway/swaylock.nix
@@ -1,48 +1,51 @@
 {...}: {
-  programs.swaylock.settings = {
-    ignore-empty-password = true;
-    font = "JetBrainsMono Nerd Font";
-    font-size = 24;
+  programs.swaylock = {
+    enable = true;
+    settings = {
+      ignore-empty-password = true;
+      font = "JetBrainsMono Nerd Font";
+      font-size = 24;
 
-    color = "161821";
-    scaling = "solid_color";
+      color = "161821";
+      scaling = "solid_color";
 
-    indicator-radius = 120;
-    indicator-thickness = 20;
+      indicator-radius = 120;
+      indicator-thickness = 20;
 
-    inside-color = "00000000";
-    inside-clear-color = "00000000";
-    inside-caps-lock-color = "00000000";
-    inside-ver-color = "00000000";
-    inside-wrong-color = "00000000";
+      inside-color = "00000000";
+      inside-clear-color = "00000000";
+      inside-caps-lock-color = "00000000";
+      inside-ver-color = "00000000";
+      inside-wrong-color = "00000000";
 
-    line-color = "6b7089";
-    line-clear-color = "e2a478";
-    line-caps-lock-color = "a093c7";
-    line-ver-color = "84a0c6";
-    line-wrong-color = "e27878";
+      line-color = "6b7089";
+      line-clear-color = "e2a478";
+      line-caps-lock-color = "a093c7";
+      line-ver-color = "84a0c6";
+      line-wrong-color = "e27878";
 
-    ring-color = "00000000";
-    ring-clear-color = "e2a478";
-    ring-caps-lock-color = "a093c7";
-    ring-ver-color = "84a0c6";
-    ring-wrong-color = "e27878";
+      ring-color = "00000000";
+      ring-clear-color = "e2a478";
+      ring-caps-lock-color = "a093c7";
+      ring-ver-color = "84a0c6";
+      ring-wrong-color = "e27878";
 
-    separator-color = "00000000";
+      separator-color = "00000000";
 
-    key-hl-color = "84a0c6";
-    bs-hl-color = "e2a478";
-    caps-lock-key-hl-color = "84a0c6";
-    caps-lock-bs-hl-color = "e2a478";
+      key-hl-color = "84a0c6";
+      bs-hl-color = "e2a478";
+      caps-lock-key-hl-color = "84a0c6";
+      caps-lock-bs-hl-color = "e2a478";
 
-    text-color = "c6c8d1";
-    text-clear-color = "e2a478";
-    text-caps-lock-color = "a093c7";
-    text-ver-color = "84a0c6";
-    text-wrong-color = "e27878";
+      text-color = "c6c8d1";
+      text-clear-color = "e2a478";
+      text-caps-lock-color = "a093c7";
+      text-ver-color = "84a0c6";
+      text-wrong-color = "e27878";
 
-    layout-bg-color = "00000000";
-    layout-border-color = "00000000";
-    layout-text-color = "c6c8d1";
+      layout-bg-color = "00000000";
+      layout-border-color = "00000000";
+      layout-text-color = "c6c8d1";
+    };
   };
 }
