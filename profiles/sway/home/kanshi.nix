66a8bd1959e35f88436f3cc62b2bda6aa31f1103
Oscar Ahlén <github@ahlen.dev>
2023-09-08 13:32:16 +0200

    Update kanshi profiles

--- profiles/sway/home/kanshi.nix
+++ profiles/sway/home/kanshi.nix
@@ -27,12 +27,26 @@
         ];
       };
 
-      op-rum-1 = {
+      op-dell-1 = {
         outputs = [
           {
-            criteria = "eDP-1";
-            status = "disable";
+            criteria = "Dell Inc. DELL U3423WE CV9MMP3";
+            status = "enable";
+            mode = "3440x1440";
+            scale = 1.0;
+            position = "0,0";
           }
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            mode = "1920x1200@59.885Hz";
+            position = "3440,0";
+          }
+        ];
+      };
+
+      op-konferensrum = {
+        outputs = [
           {
             criteria = "Samsung Electric Company SAMSUNG 0x00000F00";
             status = "enable";
@@ -40,21 +54,11 @@
             scale = 2.0;
             position = "0,0";
           }
-        ];
-      };
-
-      op-rum-gul = {
-        outputs = [
           {
             criteria = "eDP-1";
-            status = "disable";
-          }
-          {
-            criteria = "TBD";
             status = "enable";
-            mode = "3840x2160";
-            scale = 2.0;
-            position = "0,0";
+            mode = "1920x1200@59.885Hz";
+            position = "1920,0";
           }
         ];
       };
