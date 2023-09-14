446c745b4f9482dd37df3f8f5d7dcae2e1bcc5fe
Oscar Ahlén <github@ahlen.dev>
2023-09-14 10:08:05 +0200

    Add GUI config to dotnet dev shell

--- shells/dotnet/default.nix
+++ shells/dotnet/default.nix
@@ -6,13 +6,27 @@
   pkgs = import nixpkgs {inherit system;};
 in
   pkgs.mkShell {
-    packages = with pkgs; [
+    # For GUI based applications
+    LD_LIBRARY_PATH = with pkgs;
+      lib.makeLibraryPath [
+        fontconfig
+        libGL
+        libxkbcommon
+        wayland
+        xorg.libICE
+        xorg.libSM
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+      ];
+
+    buildInputs = with pkgs; [
       (with dotnetCorePackages;
         combinePackages [
           sdk_7_0
           sdk_6_0
         ])
-      fontconfig
       netcoredbg
       omnisharp-roslyn
     ];
