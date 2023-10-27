d7fd49854002d74fb1c45e77c8a8df6ac2b64327
Oscar Ahlén <github@ahlen.dev>
2023-10-27 09:56:28 +0200

    Update bat config

--- programs/bat.nix
+++ programs/bat.nix
@@ -7,14 +7,15 @@
     };
 
     themes = {
-      # nix-prefetch-url --unpack https://github.com/oahlen/iceberg.tmTheme/archive/1.0.0.tar.gz
-      iceberg = builtins.readFile (pkgs.fetchFromGitHub {
+      iceberg = {
+        src = pkgs.fetchFromGitHub {
           owner = "oahlen";
           repo = "iceberg.tmTheme";
           rev = "1.0.0";
           sha256 = "16lzyg47ddmlc2mixi2q61xpahk9jfrynp30148vkni7vhk4dbzd";
-        }
-        + "/iceberg.tmTheme");
+        };
+        file = "iceberg.tmTheme";
+      };
     };
   };
 }
