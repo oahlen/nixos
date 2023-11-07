e0b8595e8bbd1943e2d825ef81393b39aadd1abf
Oscar Ahlén <github@ahlen.dev>
2023-11-07 21:53:47 +0100

    Bump iceberg theme

--- programs/neovim/iceberg.nix
+++ programs/neovim/iceberg.nix
@@ -1,12 +1,13 @@
 {pkgs, ...}:
 pkgs.vimUtils.buildVimPlugin {
   pname = "iceberg-nvim";
-  version = "1.0.0";
+  version = "1.0.1";
   src = pkgs.fetchFromGitHub {
     owner = "oahlen";
     repo = "iceberg.nvim";
-    rev = "eae74821df5a25ddd77ef3e8e338acefe419d034";
-    sha256 = "Z/aJCeapXhM+8SyNfgz1uKlkUd9thUb7IYMmNbAGhus=";
+    rev = "5f93ea6ba3157da1907340ed8061a42f57b81abd";
+    sha256 = "+cc0P2RwBfzTVjgqX6Fj1sxl6sAE/W14CvEzvSoq0Q4=";
   };
+
   meta.homepage = "https://github.com/oahlen/iceberg.nvim/";
 }
