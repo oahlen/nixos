90d0c2f73b081da91f1e8c649ab12b3b4cff7f65
Oscar Ahlén <github@ahlen.dev>
2024-03-04 10:58:32 +0100

    Add git blame keymap

--- programs/neovim/plugins/gitsigns.lua
+++ programs/neovim/plugins/gitsigns.lua
@@ -1,10 +1,15 @@
 require "gitsigns".setup {
     signs = {
-        add          = { text = '+' },
-        change       = { text = '~' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
-    }
+        add          = { text = "+" },
+        change       = { text = "~" },
+        delete       = { text = "_" },
+        topdelete    = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked    = { text = "┆" },
+    },
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "<space>hb", function() gs.blame_line { full = true } end, opts)
+    end
 }
