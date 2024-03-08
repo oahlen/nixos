dd11b1fe19cf2bf8d0f0ab7a6849e132e5777a16
Oscar Ahlén <github@ahlen.dev>
2024-03-08 16:34:31 +0100

    Update nvim options

--- programs/neovim/init.lua
+++ programs/neovim/init.lua
@@ -6,6 +6,7 @@ vim.g.maplocalleader = ' '
 
 vim.o.autoindent = true
 vim.o.backup = false
+vim.o.breakindent = true
 vim.o.clipboard = "unnamedplus"
 vim.o.cursorline = true
 vim.o.expandtab = true
@@ -19,16 +20,18 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 10
 vim.o.shiftwidth = 4
+vim.o.shortmess = "IF"
 vim.o.showmatch = true
 vim.o.showmode = true
-vim.o.shortmess = "IF"
 vim.o.smartcase = true
 vim.o.softtabstop = 4
+vim.o.splitbelow = true
+vim.o.splitright = true
 vim.o.swapfile = false
 vim.o.tabstop = 4
 vim.o.termguicolors = true
+vim.o.undofile = true
 
-vim.opt.list = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
