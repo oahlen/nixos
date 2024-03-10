3c130ba7e43a515270de908966564a7672a145d7
Oscar Ahlén <github@ahlen.dev>
2024-03-10 13:50:42 +0100

    Don't use lsp fallback for conform

--- programs/neovim/lua/plugins/conform.lua
+++ programs/neovim/lua/plugins/conform.lua
@@ -7,6 +7,6 @@ require("conform").setup({
     format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_fallback = false,
     },
 })
