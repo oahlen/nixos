8711d90ee7567cf103d2f42d95b570189779bab3
Oscar Ahlén <github@ahlen.dev>
2024-03-11 19:59:35 +0100

    Add nix gc option

--- system/nix.nix
+++ system/nix.nix
@@ -9,5 +9,11 @@
       keep-outputs = true
       keep-derivations = true
     '';
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
   };
 }
