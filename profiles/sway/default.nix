40515d8ea8fe3560f344784d88c454f02dd2477e
Oscar Ahlén <github@ahlen.dev>
2024-06-09 15:35:45 +0200

    Fix sway session variables

--- profiles/sway/default.nix
+++ profiles/sway/default.nix
@@ -2,15 +2,6 @@
   programs.sway = {
     enable = true;
 
-    extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-      export _JAVA_AWT_WM_NONREPARENTING=1
-      export MOZ_ENABLE_WAYLAND=1
-      export NIXOS_OZONE_WL=1
-    '';
-
     extraPackages = with pkgs; [
       brightnessctl
       glib
